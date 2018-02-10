9809d7788829b7088dd9084740244081839e6f69
Yourong Ye <34338026+YourongYe@users.noreply.github.com>
2018-02-10 20:10:15 +0800

    Update list.py

--- list.py
+++ list.py
@@ -1,27 +1,30 @@
 # LIST
 
-a = [1,2,3,4,5,6]
-print(a)
+a = 2
+b = 3.5
+c = a*b
+print (c)
+
+a = [1,2,3,4,5,6] #list,相当于C++里的vector
+print (a)
 
 b = []
 b.append(2)
 b.append(3)
 b.append(4)
-print(b)
+print (b)
 
-c = list(range(1,10))
-print(c)
+c = list(range(1,10)) #初始化list
+print (c)
 
-d = list(range(10))
-print(d)
+d = list(range(10)) #默认从0开始，默认间隔是1
+print (d)
 
-e = list(range(1,10,2))
-print(e)
+e = list(range(1,10,2)) #最后一个是间隔
+print (e)
 
 f = [1,2,5,2,6,2,7]
-# C++
-# for(int i=0; i<f.size(); i++){
-#    std::cout<<f[i]*2<<std::endl;  
-# }
-for x in f:
-  print(x*2)
+for x in f: #for循环，格式很重要，要空两格才属于循环里的东西
+  print (x*2)
+
+
