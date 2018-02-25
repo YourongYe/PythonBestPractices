9d654703e6c790d98a35434ff25970c262e77ea8
Yourong Ye <34338026+YourongYe@users.noreply.github.com>
2018-02-25 11:36:54 +0000

    Update class.py

--- class.py
+++ class.py
@@ -51,7 +51,31 @@ class StudentInfo:
 
 b = StudentInfo("yourong","SW1", 123, 100)
 
-
 print(b.name)
 print(b.address)
 print(b.phone)
+
+#####################################################################
+# Class with "changeAddress" member function
+#####################################################################
+
+class StudentInfo:
+  name = ""
+  address = ""
+  phone = 0
+  score = 0
+  
+  def __init__(self, in_name, in_address, in_phone, in_score):
+    self.name = in_name
+    self.address = in_address
+    self.phone = in_phone
+    self.score = in_score
+    
+  def changeAddress(self, new_address):
+    self.address = new_address
+
+b = StudentInfo("yourong","SW1", 123, 100)
+
+print(b.address)
+b.changeAddress("N1")
+print(b.address)
