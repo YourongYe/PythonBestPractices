67d81043aeeae10db86ce0f5c118b7dea3c70629
Yourong Ye <34338026+YourongYe@users.noreply.github.com>
2018-05-20 15:09:30 +0800

    Update DataFrame.py

--- Pandas/DataFrame.py
+++ Pandas/DataFrame.py
@@ -1,4 +1,10 @@
+import pandas as pd
+import numpy as np
+from pandas import Series,DataFrame
+
 ############################ index ##################################
 # 1. set_index
+df = DataFrame()
 data.set_index('column_name') # specify a column to be index
-data.index = range(1,len(data))
+data.index = range(0,len(data)) # note that range(a,b) means a <= x & x < b.
+data.
