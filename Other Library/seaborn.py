1e05e1ff365402f431ddaed51288406b3e8fe4de
Yourong Ye <34338026+YourongYe@users.noreply.github.com>
2018-05-31 22:13:10 +0800

    Update seaborn.py

--- Other Library/seaborn.py
+++ Other Library/seaborn.py
@@ -1,3 +1,9 @@
 import seaborn as sns 
 # Seaborn 可以很方便的画出直方图，拟合曲线 (密度分布函数)
 sns.distplot(data, rug=True, hist=False)
+# 和用matplotlib画直方图类似
+sns.distplot(df['column_name'], hist=True, kde=False, bins=80, color = 'blue',hist_kws={'edgecolor':'black'})
+# Add labels
+plt.title('Histogram of Arrival Delays')
+plt.xlabel('Delay (min)')
+plt.ylabel('Flights')
