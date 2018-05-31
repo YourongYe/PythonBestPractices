ad4fd22a471e9cdf1d3ac5b79746908d08516f74
Yourong Ye <34338026+YourongYe@users.noreply.github.com>
2018-05-31 22:09:30 +0800

    Update matplotlib.py

--- Other Library/matplotlib.py
+++ Other Library/matplotlib.py
@@ -1,2 +1,6 @@
 # Histogram 为最常用的查看密度分布可视化方法，利用matplotlib 中 hist 函数即可
 plt.hist(data, bins=50, color='steelblue', normed=True )
+plt.show()
+# python会自动帮你分出每组的频率有多少个，然后再画图就可以了。bins 指的是直方条的个数
+plt.hist(df['column_name'], color = 'blue', edgecolor = 'black', bins = 100)
+plt.show()
