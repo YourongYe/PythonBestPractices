b4cd08248a04662b13dab7917ab86ac7c73ce7b7
Yourong Ye <34338026+YourongYe@users.noreply.github.com>
2018-05-31 22:29:13 +0800

    Update seaborn.py

--- Other Library/seaborn.py
+++ Other Library/seaborn.py
@@ -1,5 +1,5 @@
 import seaborn as sns 
-# Seaborn 可以很方便的画出直方图，拟合曲线 (密度分布函数)
+# Seaborn 可以很方便的画出直方图，并且可以选择同时画出拟合曲线rug (密度分布函数)
 sns.distplot(data, rug=True, hist=False)
 # 和用matplotlib画直方图类似
 sns.distplot(df['column_name'], hist=True, kde=False, bins=80, color = 'blue',hist_kws={'edgecolor':'black'})
